45626a35e036b9df955927b4ff3d6ffc00ded611
Jonathan Unikowski <jonathan@unikowski.net>
2019-01-29 11:23:01 -0800

    remove unnecessary exports

--- main.js
+++ main.js
@@ -2,22 +2,26 @@ const config = {
   data: {
     startYear: 1991,
     // period in years
-    // code doesn't handle period that are not a multiple of the monthBlockSize well
+    // code doesn't handle period that are not a multiple of the yearBlockSize well
     period: 40,
+    yearBlockSize: 10,
     //monthsLabels: [{name: "January", maxDays: 31}, {name: "February", maxDays: 29}, {name: "March", maxDays: 31}, {name: "April", maxDays: 30}, {name: "May", maxDays: 31}, {name: "June", maxDays: 30}, {name: "July", maxDays: 31}, {name: "August", maxDays: 31}, {name: "September", maxDays: 30}, {name: "October", maxDays: 31}, {name: "November", maxDays: 30}, {name: "December", maxDays: 31}],
     // For dev purposes
     monthsLabels: [{name: "January", maxDays: 31}, {name: "February", maxDays: 29}, {name: "March", maxDays: 31}],
   },
   styling : {
+    // font needs to be installed locally or referenced in html declarations
+    font: 'Source Sans Pro',
+    // standard day size
     daySize: 20,
+    // spacing in between day blocks
     interBoxPadding: 3,
     textSize: "14",
-    // TODO: change monthblocksize to something clearer (eg yearblocksize)
-    monthBlockSize: 10,
+    monthLabelsHeight: 20,
+    // borders
     calendarBorderThickness: 6,
     innerGridBorderThickness: 2,
     innerCellBorderThickness: 1,
-    monthLabelsHeight: 20,
     // month-level vertical and horizontal padding
     vPadding: 2,
     hPadding: 6,
